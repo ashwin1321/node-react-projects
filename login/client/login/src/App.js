e2f28706a959e8836635d74bab72c1b4186cb9a0
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-10-18 14:38:09 +0545

    homepage updated

--- login/client/login/src/App.js
+++ login/client/login/src/App.js
@@ -1,6 +1,6 @@
 // import './App.css';
 import Homepage from "./Components/Homepage.js"
-
+// import Login from "./Login.js"
 
 function App() {
   return (
--- login/client/login/src/Components/Homepage.js
+++ login/client/login/src/Components/Homepage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../Styles/Homepage.css'
-import Login from "./Login.js"
+
 
 const Homepage = () => {
 
